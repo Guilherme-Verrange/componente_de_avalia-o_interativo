dae7c85f159f2e1895d0fc517978abc17c093067
Guilherme Verrange <60721610+Guilherme-Verrange@users.noreply.github.com>
2022-08-05 14:03:24 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -8,25 +8,36 @@ const show_score = document.querySelector('.show-score');
 
 
 submit_btn.addEventListener('click', onSubmit);
-rate_btn.forEach( btn =>{
+rate_btn.forEach(btn => {
     btn.addEventListener('click', click_btn_rate);
 });
 
-function onSubmit(){
+function onSubmit() {
 
-    card.classList.add("hide");
-    show_score.textContent = score_rate;
-    card_2.classList.remove("hide");
+    console.log(onSubmit);
+    console.log(score_rate);
+
+    if (score_rate == "SUBMIT") {
+        alert('Escolha uma nota para avaliar');
+        setTimeout(function () {
+            window.location.href = "index.html";
+        }, 0000);
+    }
+    else {
+        card.classList.add("hide");
+        show_score.textContent = score_rate;
+        card_2.classList.remove("hide");
+    }
 }
 
-function click_btn_rate(event){
-    rate_btn.forEach(btn =>{
+function click_btn_rate(event) {
+    rate_btn.forEach(btn => {
         btn.classList.remove('active');
     });
     event.target.classList.add('active');
 
     score_rate = event.target.textContent;
-    console.log(score);
+
 
 };
 
